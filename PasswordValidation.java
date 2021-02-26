03e8d4692f041241462fb25febb9ac987e36a6d3
Junhee Kim <juki207@ess.ais.ac.nz>
2021-02-26 23:03:09 +1300

    modified java file 0226

--- PasswordValidation.java
+++ PasswordValidation.java
@@ -3,7 +3,7 @@ public class PasswordValidation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		validatePwd validpwd = new validatePwd();
+		ValidatePwd validpwd = new ValidatePwd();
 		
 		String password = "sfsdffsdTsdf";
 		
--- test0212.java
+++
@@ -1,7 +0,0 @@
-public class Test{
-
-	public void main(String[] args){
-		System.out.println("Testing Git pushing repository");		
-	}
-
-}
